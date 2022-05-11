01dbcdf607083b59f704b4366f97d40805d91665
AlAminKh03 <alaminkhan6203@gmail.com>
2022-05-11 22:50:58 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Header from './Pages/Shared/Header/Header';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
